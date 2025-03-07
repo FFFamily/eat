d247c28906351163945e47102053a875c69000d9
tutu <tjj747487547@sina.com>
2025-03-07 09:05:56 +0800

    feat: 食物

---
+++ web/admin-ui/src/views/mobile/food_list/WhatToEat.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="what-to-eat-page">
+    <!-- 设置图标按钮 -->
+    <el-button text  size="large" :icon="Operation" class="setting-button" @click="openDrawer">
+      <!-- <el-icon><Operation /></el-icon> -->
+    </el-button>
+    <!-- 抽屉 -->
+    <el-drawer v-model="drawerVisible" title="设置" direction="ltr" size="80%">
+      <!-- 这里可以添加抽屉里的设置内容，比如食物列表的编辑等 -->
+      <template>
+        <p>这里可以放设置相关的内容</p>
+      </template>
+      <template #footer>
+        <el-button @click="drawerVisible = false">关闭</el-button>
+      </template>
+    </el-drawer>
+    <!-- 巨大圆形按钮 -->
+    <el-button class="big-round-button" @click="openFoodSelection">今天吃什么</el-button>
+    <!-- 食物选择对话框 -->
+    <el-dialog v-model="dialogVisible" title="选择食物">
+      <ul>
+        <li v-for="food in foodList" :key="food" @click="selectFood(food)">{{ food }}</li>
+      </ul>
+      <template #footer>
+        <el-button @click="dialogVisible = false">取消</el-button>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Operation } from '@element-plus/icons-vue'
+// 控制对话框显示状态
+const dialogVisible = ref(false);
+
+// 控制抽屉显示状态
+const drawerVisible = ref(false);
+
+// 食物列表
+const foodList = ref([
+  '汉堡',
+  '披萨',
+  '寿司',
+  '拉面',
+  '火锅'
+]);
+
+// 打开食物选择对话框
+const openFoodSelection = () => {
+  dialogVisible.value = true;
+};
+
+// 选择食物
+const selectFood = (food) => {
+  // 这里可以添加选择食物后的逻辑，比如显示提示信息等
+  console.log(`你选择了: ${food}`);
+  dialogVisible.value = false;
+};
+
+// 打开抽屉
+const openDrawer = () => {
+  drawerVisible.value = true;
+};
+</script>
+
+<style scoped>
+.what-to-eat-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  position: relative;
+}
+
+.big-round-button {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  font-size: 24px;
+  background-color: #409EFF;
+  color: white;
+  border: none;
+}
+
+.setting-button {
+  position: absolute;
+  top: 1px;
+  left: 1px;
+}
+</style>
---
+++ web/admin-ui/src/views/mobile/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <WhatToEat />
+  </div>
+</template>
+
+<script setup>
+import WhatToEat from './food_list/WhatToEat.vue';
+</script>
+
+<style scoped>
+/* 可以添加额外的样式 */
+</style>
